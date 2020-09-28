8cfd6c7ff97d4041b780f690a8246adaf0510496
yangjunjie <yangjunjie@MacBook-Pro.local>
2020-09-28 15:31:10 -0500

    update pdf

---
+++ script.sh
@@ -0,0 +1,14 @@
+alias math_kernel=/Applications/Mathematica.app/Contents/MacOS/MathKernel
+export cur_path=$(pwd)
+export nb_path=$cur_path/notebook/
+export pdf_path=$cur_path/pdf/
+for file in $(ls ./notebook/ | grep nb)
+do
+    filename=${file%.*}
+    echo filename=$filename
+    pdfname=$pdf_path$filename.pdf
+    echo pdfname=$pdfname
+    nbname=$nb_path$filename.nb
+    echo nbname=$nbname
+    math_kernel -run "SetOptions[First[\$Output],FormatType->StandardForm];UsingFrontEnd[NotebookEvaluate[\"$nbname\",InsertResults->True];nb=NotebookOpen[\"$nbname\"];Export[\"$pdfname\",nb]];Exit[]"
+done
