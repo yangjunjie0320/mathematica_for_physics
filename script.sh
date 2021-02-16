dc3e503036ed9ecfcfd5ab76d8fbbb1fa76e5c52
Junjie Yang <yangjunjie0320@gmail.com>
2021-02-16 07:04:33 -0600

    update

--- script.sh
+++ script.sh
@@ -1,7 +1,18 @@
-alias math_kernel=/Applications/Mathematica.app/Contents/MacOS/MathKernel
+alias  math_kernel=/Applications/Mathematica.app/Contents/MacOS/MathKernel
 export cur_path=$(pwd)
 export nb_path=$cur_path/notebook/
 export pdf_path=$cur_path/pdf/
+export src_path=$cur_path/src/
+
+for dir in $(ls ./src/)
+do 
+    dir_name=$src_path$dir
+    echo dir_name=$dir_name
+    cd $dir_name
+    make
+    cd -
+done
+
 for file in $(ls ./notebook/ | grep nb)
 do
     filename=${file%.*}
